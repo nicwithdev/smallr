5093650f6171c2327878e023afa6465ca2a74f4b
Nicole WItherall <nicwitherall@gmail.com>
2021-03-24 18:00:04 -0400

    add responsiveness

--- public/javascripts/pets-script.js
+++ public/javascripts/pets-script.js
@@ -3,7 +3,20 @@ const menu = document.querySelector('.options');
 const list = document.querySelector('.category-list');
 const listActive = document.querySelector('.list-active');
 
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+console.log(nav)
+console.log(burger)
+console.log(menu)
+
 // event listeners
+
+burger.addEventListener('click', function() {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('exit');
+});
+
 menu.addEventListener('click', function() {
     list.classList.toggle('list-active');
 });
+
